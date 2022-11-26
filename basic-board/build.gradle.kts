8be58123bbdb20b7f48f5a1afac2b52ccb2b07f3
hou27 <ataj125@gmail.com>
2022-11-27 03:14:41 +0900

    feat: querydsl 설정
    gradle/groovy로 변경, spring boot 버전을 2.7.6으로 낮춰
    최대한 인터넷에 존재하는 자료들과 맞춘 후 재시도하여
    querydsl build에 성공
    
    querydsl을 위해 build.gradle에 설정해준 후 build하여
    src/main/generated에 QClass를 생성함
    기본 경로는 build 하위이지만 src 하위로 꺼내온 이유는,
    intellij를 사용하면서 빌드 시 gradle의 스캔범위와 intellij가 스캔하는 범위에
    차이가 있어 QClass를 중복 스캔하며 충돌나는 에러가 발생할 수 있어
    이와 같이 처리함.

--- basic-board/build.gradle.kts
+++ basic-board/build.gradle.kts
@@ -2,6 +2,13 @@ plugins {
     java
     id("org.springframework.boot") version "3.0.0"
     id("io.spring.dependency-management") version "1.1.0"
+
+    // querydsl을 위한 추가 플러그인
+    val kotlinVersion = "1.7.10"
+    // KAPT(Kotlin Annotation Processing Tool)를 설치
+    kotlin("kapt") version kotlinVersion
+    // Intellij에서 사용할 파일을 생성하는 플러그인
+    idea
 }
 
 group = "com.hou27"
@@ -30,8 +37,35 @@ dependencies {
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // queryDSL 설정
+    implementation("com.querydsl:querydsl-jpa")
+    implementation("com.querydsl:querydsl-core")
+    implementation("com.querydsl:querydsl-collections")
+//    annotationProcessor("com.querydsl:querydsl-apt:${dependencyManagement.importedProperties["querydsl.version"]}:jpa") // querydsl JPAAnnotationProcessor 사용 지정
+    kapt("com.querydsl:querydsl-apt:5.0.0:jpa") // querydsl JPAAnnotationProcessor 사용 지정
+    annotationProcessor("jakarta.annotation:jakarta.annotation-api") // java.lang.NoClassDefFoundError (javax.annotation.Generated) 대응 코드
+    annotationProcessor("jakarta.persistence:jakarta.persistence-api") // java.lang.NoClassDefFoundError (javax.annotation.Entity) 대응 코드
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+
+// Querydsl 설정부
+
+/**
+ * gradle kotlin version
+ */
+
+idea {
+    module {
+//        val generated = file("src/main/generated")
+        val generated = file("build/generated/source/kapt/main")
+        // java source set 에 querydsl QClass 위치 추가
+        sourceDirs.add(generated)
+        // querydsl QClass 파일 생성 위치를 지정
+        generatedSourceDirs.add(generated)
+    }
 }
